371a511fee907896db8591ed06564a849c4dd3eb
LeoWu343 <leowu@berkeley.edu>
2013-02-09 17:25:10 -0800

    stuff

--- Wall.py
+++ Wall.py
@@ -1,9 +1,8 @@
-import random
-class Ant:
-    def __init__(self, x_coord, y_coord, imagedata):
+class Wall:
+    def __init__(self, x_coord, y_coord):
         self.x = x_coord
         self.y = y_coord
-        self.imagedata = "R0lGODlhEAAQAPcAAAAAAAAAMwAAZgAAmQAAzAAA/wArAAArMwArZgArmQArzAAr/wBVAABVMwBV
+        imagedata = "R0lGODlhEAAQAPcAAAAAAAAAMwAAZgAAmQAAzAAA/wArAAArMwArZgArmQArzAAr/wBVAABVMwBV
 ZgBVmQBVzABV/wCAAACAMwCAZgCAmQCAzACA/wCqAACqMwCqZgCqmQCqzACq/wDVAADVMwDVZgDV
 mQDVzADV/wD/AAD/MwD/ZgD/mQD/zAD//zMAADMAMzMAZjMAmTMAzDMA/zMrADMrMzMrZjMrmTMr
 zDMr/zNVADNVMzNVZjNVmTNVzDNV/zOAADOAMzOAZjOAmTOAzDOA/zOqADOqMzOqZjOqmTOqzDOq
@@ -17,28 +16,5 @@ zJn//8wAAMwAM8wAZswAmcwAzMwA/8wrAMwrM8wrZswrmcwrzMwr/8xVAMxVM8xVZsxVmcxVzMxV
 AMz/M8z/Zsz/mcz/zMz///8AAP8AM/8AZv8Amf8AzP8A//8rAP8rM/8rZv8rmf8rzP8r//9VAP9V
 M/9VZv9Vmf9VzP9V//+AAP+AM/+AZv+Amf+AzP+A//+qAP+qM/+qZv+qmf+qzP+q///VAP/VM//V
 Zv/Vmf/VzP/V////AP//M///Zv//mf//zP///wAAAAAAAAAAAAAAACH5BAEAAPwALAAAAAAQABAA
-AAhGAPcJ3Adg4MCCBg8mJGgQocCCACA2ZHgwokOKFC1afJhQo8eLHTcuDBlx4ceTIitqHNkwpUmU
-IBF+5EiToEeFMV1ibDkyIAA7"
-
-    def move(self, new_x, new_y):
-        self.x = new_x
-        self.y = new_y
-
-    def make_rand_move(self):
-        foo = random.randint(0, 7)
-        if foo==0:
-            move(self.x-1, self.y-1)
-        elif foo==1:
-            move(self.x, self.y-1)
-        elif foo==2:
-            move(self.x+1, self.y-1)
-        elif foo==3:
-            move(self.x+1, self.y)
-        elif foo==4:
-            move(self.x+1, self.y+1)
-        elif foo==5:
-            move(self.x, self.y+1)
-        elif foo==6:
-            move(self.x-1, self.y+1)
-        else:
-            move(self.x-1, self.y)
+AAg8AN0JHEiwoMGDCBMapDeQoUCHCNsRlGiQIkKIGBsqHGjRXceNB+dpfAjS40GIIDu2y1jS5MWN
+DjuidBcQADs="
